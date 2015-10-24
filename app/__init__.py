2d519e041bbf7208298bcce95cf7ff3223ddbb50
Robin Hutmacher <robin.hutmacher@gmail.com>
2015-10-24 11:19:39 +0200

    Added basic index.html and bootstrap. Runnable version

--- app/__init__.py
+++ app/__init__.py
@@ -2,8 +2,9 @@ import os
 import sys
 
 from flask import Flask, render_template
-from flask_restful import Resource, Api
 
 app = Flask(__name__)
-app.config.from_object('config')
+#app.config.from_object('config')
+
+from app import views
 
