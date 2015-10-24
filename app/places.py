90db5f6e7909cf2731b8875e1b9fac1b2a525c7b
Valentin Rublack <valentin.rublack@gmail.com>
2015-10-24 19:52:45 +0200

    Places are sorted by number of reviews (indicating popularity) in descending order

--- app/places.py
+++ app/places.py
@@ -6,6 +6,12 @@ from app import app
 from math import sin, cos, sqrt, atan2, radians
 import random
 
+def my_key(place):
+    if "reviews" in place.details:
+        return len(place.details["reviews"])
+    else:
+        return 0
+
 google_places = GooglePlaces(app.config['API_KEY'])
 city = "Karlsruhe, Germany"
 keywords = "Shopping"
@@ -13,6 +19,14 @@ query_results = google_places.nearby_search(
         location=city, 
         keyword=keywords,
         radius=20000)
+
+# has to be called in order for the details to be fetched
+for p in query_results.places:
+    p.get_details()
+
+# sorted by number of reviews in descending order
+sorted_results = sorted(query_results.places, key=my_key, reverse=True)
+
 
 def calculate_distance(guessed_location, actual_location):
     # approximate radius of earth in km
@@ -36,5 +50,5 @@ def calculate_distance(guessed_location, actual_location):
     return distance
 
 def get_random_location():
-    random_place = random.choice(query_results.places)
+    random_place = random.choice(sorted_results)
     return [random_place.name, random_place.geo_location]
